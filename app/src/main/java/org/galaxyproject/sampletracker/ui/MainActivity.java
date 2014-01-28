4be1900a374c78d431948bdd0c6986e22c7861d0
Pavel Sveda <pavel.sveda@cleverlance.com>
2014-01-28 21:23:17 +0100

    Main activity UI

---
+++ app/src/main/java/org/galaxyproject/sampletracker/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package org.galaxyproject.sampletracker.ui;
+
+import org.galaxyproject.sampletracker.R;
+import org.galaxyproject.sampletracker.ui.core.BaseActivity;
+
+import roboguice.inject.ContentView;
+
+/**
+ * Landing activity with main menu.
+ * 
+ * @author Pavel Sveda <xsveda@gmail.com>
+ */
+@ContentView(R.layout.act_main)
+public final class MainActivity extends BaseActivity {
+}
