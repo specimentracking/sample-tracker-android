bd91632169916bdf7d18b7cfab205d33f2fee988
Pavel Sveda <pavel.sveda@cleverlance.com>
2014-02-04 19:23:08 +0100

    Add galaxy project server environment

---
+++ app/src/samples/java/org/galaxyproject/sampletracker/Environment.java
@@ -0,0 +1,15 @@
+package org.galaxyproject.sampletracker;
+
+/**
+ * Environment configuration.
+ * 
+ * @author Pavel Sveda <xsveda@gmail.com>
+ */
+public final class Environment {
+
+    /** Base URL of the GALAXY server */
+    public static final String GALAXY_URL = "http://samples.galaxyproject.org";
+
+    /** Flag indicating whether GALAXY server certificate and hostname should be verified */
+    public static final boolean GALAXY_SSL_VERIFY = true;
+}
