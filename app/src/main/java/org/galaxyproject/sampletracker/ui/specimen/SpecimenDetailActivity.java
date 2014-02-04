e2803de63a9ca0b2faba2387e7df703634a28911
Pavel Sveda <xsveda@gmail.com>
2014-02-04 22:32:26 +0100

    Call specimen check when detail is shown

--- app/src/main/java/org/galaxyproject/sampletracker/ui/specimen/SpecimenDetailActivity.java
+++ app/src/main/java/org/galaxyproject/sampletracker/ui/specimen/SpecimenDetailActivity.java
@@ -3,20 +3,26 @@ package org.galaxyproject.sampletracker.ui.specimen;
 import android.content.Intent;
 import android.os.Bundle;
 
-import org.galaxyproject.sampletracker.GalaxyApplication;
-import org.galaxyproject.sampletracker.R;
-import org.galaxyproject.sampletracker.ui.core.BaseActivity;
+import com.google.inject.Inject;
 
-import roboguice.inject.ContentView;
+import org.galaxyproject.sampletracker.GalaxyApplication;
+import org.galaxyproject.sampletracker.logic.galaxy.SpecimenResourceController;
+import org.galaxyproject.sampletracker.model.galaxy.specimen.SpecimenResponse;
+import org.galaxyproject.sampletracker.ui.core.BaseActivity;
+import org.galaxyproject.sampletracker.util.Toasts;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 import roboguice.inject.InjectExtra;
+import roboguice.util.Ln;
 
 /**
  * Detail of single specimen, either existing, new or derivative.
  * 
  * @author Pavel Sveda <xsveda@gmail.com>
  */
-@ContentView(R.layout.act_specimen_detail)
-public final class SpecimenDetailActivity extends BaseActivity {
+public final class SpecimenDetailActivity extends BaseActivity implements Callback<SpecimenResponse> {
 
     /** Barcode value of the specimen */
     private static final String EXTRA_BARCODE = "barcode";
@@ -28,15 +34,33 @@ public final class SpecimenDetailActivity extends BaseActivity {
     }
 
     @InjectExtra(EXTRA_BARCODE) private String mBarcode;
+    @Inject private SpecimenResourceController mSpecimenController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         showHomeButton();
+
+        if (savedInstanceState == null) {
+            mSpecimenController.check(mBarcode, this);
+        }
     }
 
     @Override
     public void onBackPressed() {
         upToParent();
+    }
+
+    @Override
+    public void success(SpecimenResponse specimenResponse, Response response) {
+        // TODO
+        Toasts.showShort("Success " + specimenResponse.getSpecimen());
+    }
+
+    @Override
+    public void failure(RetrofitError error) {
+        // TODO - 404 json only
+        Ln.w(error);
+        Toasts.showShort("Error " + error.getResponse().getStatus());
     }
 }
