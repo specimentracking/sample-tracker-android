35a90b903cf70a6acc95b724ebaa178586f69a78
Pavel Sveda <xsveda@gmail.com>
2014-02-23 18:38:50 +0100

    Show all specimen details info

--- app/src/main/java/org/galaxyproject/sampletracker/ui/specimen/SpecimenDetailsFragment.java
+++ app/src/main/java/org/galaxyproject/sampletracker/ui/specimen/SpecimenDetailsFragment.java
@@ -16,7 +16,10 @@ import org.galaxyproject.sampletracker.R;
 import org.galaxyproject.sampletracker.model.galaxy.specimen.SampleData;
 import org.galaxyproject.sampletracker.model.galaxy.specimen.Specimen;
 import org.galaxyproject.sampletracker.ui.core.BaseFragment;
+import org.galaxyproject.sampletracker.util.DateTimeUtils;
 import org.galaxyproject.sampletracker.util.ViewUtils;
+
+import java.util.Date;
 
 /**
  * Detailed read-only information about a specimen.
@@ -36,8 +39,6 @@ public final class SpecimenDetailsFragment extends BaseFragment {
         fragment.setArguments(args);
         return fragment;
     }
-
-    private TextView mFamilyValue;
 
     private Specimen mSpecimen;
 
@@ -75,21 +76,30 @@ public final class SpecimenDetailsFragment extends BaseFragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        bindViews(view);
-        bindModel(mSpecimen);
+        SampleData sampleData = mSpecimen.getSampleData();
+
+        write(view, R.id.family, sampleData.getFamily());
+        write(view, R.id.sex, sampleData.getSex());
+        write(view, R.id.participant_relationship, sampleData.getParticipantRelationship());
+        write(view, R.id.participant_dob, sampleData.getParticipantDob());
+        write(view, R.id.derivative, sampleData.getParentId() == null ? R.string.glb_no : R.string.glb_yes);
+        write(view, R.id.create_time, mSpecimen.getCreateTime());
+        write(view, R.id.update_time, mSpecimen.getUpdateTime());
+        write(view, R.id.collection_date, sampleData.getCollectionDate());
+        write(view, R.id.sent_date, sampleData.getSentDate());
+        write(view, R.id.note, sampleData.getNote());
     }
 
-    private void bindViews(View view) {
-        mFamilyValue = (TextView) view.findViewById(R.id.family);
+    private void write(View root, int viewId, int resId) {
+        write(root, viewId, getString(resId));
     }
 
-    private void bindModel(Specimen specimen) {
-        SampleData sampleData = specimen.getSampleData();
-
-        write(mFamilyValue, sampleData.getFamily());
+    private void write(View root, int viewId, Date date) {
+        write(root, viewId, DateTimeUtils.formatAsDateTime(date));
     }
 
-    private void write(TextView view, String value) {
+    private void write(View root, int viewId, String value) {
+        TextView view = (TextView) root.findViewById(viewId);
         ViewUtils.conditionalGone(!TextUtils.isEmpty(value), (View) view.getParent());
         view.setText(value);
     }
