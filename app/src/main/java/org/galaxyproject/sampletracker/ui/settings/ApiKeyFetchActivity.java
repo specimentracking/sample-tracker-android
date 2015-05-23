5eea4add02c44a55398d5b9c095d5fe9f84aa350
Pavel Sveda <xsveda@gmail.com>
2015-05-23 20:37:46 +0200

    Add activity to fetch an API key

---
+++ app/src/main/java/org/galaxyproject/sampletracker/ui/settings/ApiKeyFetchActivity.java
@@ -0,0 +1,11 @@
+package org.galaxyproject.sampletracker.ui.settings;
+
+import org.galaxyproject.sampletracker.ui.core.BaseActivity;
+
+/**
+ * Fetches an API key automatically based on username a password.
+ *
+ * @author Pavel Sveda <xsveda@gmail.com>
+ */
+public final class ApiKeyFetchActivity extends BaseActivity {
+}
